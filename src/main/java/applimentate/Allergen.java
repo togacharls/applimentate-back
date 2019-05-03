6e1aaa2a169aa43c64f72b95ccd1296570996fcc
togacharls <togacharls@gmail.com>
2019-05-03 17:15:09 +0200

    feature: GET allergens

---
+++ src/main/java/applimentate/Allergen.java
@@ -0,0 +1,14 @@
+package applimentate;
+
+public class Allergen {
+    private String id;
+
+    public Allergen(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
+
---
+++ src/main/java/applimentate/AllergensController.java
@@ -0,0 +1,38 @@
+package applimentate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class AllergensController {
+
+    @RequestMapping(value = "/allergens", method = GET)
+    public List<Allergen> getAllergens() {
+        return this.getAllergenList();
+    }
+
+    private List<Allergen> getAllergenList() {
+        final Allergen[] allergens = {
+                new Allergen("LUPINS"),
+                new Allergen("CELERY"),
+                new Allergen("PEANUTS"),
+                new Allergen("CRUSTACEANS"),
+                new Allergen("SULFUR_DIOXIDE_AND_SULPHITES"),
+                new Allergen("NUTS"),
+                new Allergen("GLUTEN"),
+                new Allergen("SESAME_SEEDS"),
+                new Allergen("EGG"),
+                new Allergen("DAIRY_PRODUCTS"),
+                new Allergen("MOLLUSCS"),
+                new Allergen("MUSTARD"),
+                new Allergen("FISH"),
+                new Allergen("SOY")
+        };
+        return Arrays.asList(allergens);
+    }
+}
